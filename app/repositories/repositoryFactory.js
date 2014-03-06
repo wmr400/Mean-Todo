4e59b05f761fb84f9378b33dea4700a005a621a6
Wouter Mandemaker <wmr400@gmail.com>
2014-03-06 11:16:12 +0100

    Added logging.

--- app/repositories/repositoryFactory.js
+++ app/repositories/repositoryFactory.js
@@ -1,4 +1,6 @@
 module.exports.create = function(modelName) {
-	var Repository = require('./' + modelName.toLowerCase() + 'Repository');
+	var repositoryName = modelName.toLowerCase() + 'Repository';
+	console.log('Will create new ' + repositoryName + '...');
+	var Repository = require('./' + repositoryName);
 	return new Repository();
 };

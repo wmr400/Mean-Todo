94769cc045f4a092108a0929c643afc158f6a345
Wouter Mandemaker <wmandemaker@gmail.com>
2014-03-07 08:56:48 +0100

    Changed code layout in main.js. Added call to PersonService.get() (in comments).

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,38 +1,15 @@
 angular.module('todoApp.controllers')
 
-	.controller('MainController', ['$scope', '$http', 'TodoService', function($scope, $http, TodoService) {
-		$scope.formData = {};
+	.controller('MainController', [
+		'$scope', 
+		'$http', 
+		'TodoService', 
+		'PersonService' 
+		function($scope, $http, TodoService, PersonService) {
+			$scope.formData = {};
 
-		// We've landed on the home page, retrieve all todos and display them.
-		TodoService.get()
-			.success(function(data) {
-				$scope.todos = data;
-				console.log(data);
-			})
-			.error(function(data) {
-				console.log('Error: ' + data);
-			});
-
-		// when submitting the add form, send the text to the node API
-		$scope.createTodo = function() {
-
-			if (!$.isEmptyObject($scope.formData)) {
-
-				TodoService.create($scope.formData)
-					.success(function(data) {
-						$scope.formData = {}; // clear the form so our user is ready to enter another
-						$scope.todos = data;
-						console.log(data);
-					})
-					.error(function(data) {
-						console.log('Error: ' + data);
-					});
-			}
-		};
-
-		// delete a todo after checking it
-		$scope.deleteTodo = function(id) {
-			TodoService.delete(id)
+			// We've landed on the home page, retrieve all todos and display them.
+			TodoService.get()
 				.success(function(data) {
 					$scope.todos = data;
 					console.log(data);
@@ -40,5 +17,42 @@ angular.module('todoApp.controllers')
 				.error(function(data) {
 					console.log('Error: ' + data);
 				});
-		};
-}]);
+
+			// PersonService.get()
+			// 	.success(function(data) {
+			// 		$scope.persons = data;
+			// 		console.log(data);
+			// 	})
+			// 	.errror(function(data) {
+			// 		console.log('Error: ' + data);
+			// 	});
+
+			// when submitting the add form, send the text to the node API
+			$scope.createTodo = function() {
+
+				if (!$.isEmptyObject($scope.formData)) {
+
+					TodoService.create($scope.formData)
+						.success(function(data) {
+							$scope.formData = {}; // clear the form so our user is ready to enter another
+							$scope.todos = data;
+							console.log(data);
+						})
+						.error(function(data) {
+							console.log('Error: ' + data);
+						});
+				}
+			};
+
+			// delete a todo after checking it
+			$scope.deleteTodo = function(id) {
+				TodoService.delete(id)
+					.success(function(data) {
+						$scope.todos = data;
+						console.log(data);
+					})
+					.error(function(data) {
+						console.log('Error: ' + data);
+					});
+			};
+	}]);

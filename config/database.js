f1190ae0bda8a57344d3c4ed823a2d90f92979c5
Wouter Mandemaker <wmandemaker@gmail.com>
2014-03-21 16:28:45 +0100

    Revert "Attemtps at getting that stupid alert full width at the top."
    This reverts commit 41421386ed7dab75ae502977dd1f03a6d83d2447.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,3 @@
 module.exports = {
-	// url: 'mongodb://dbuser:dbpass@ds033629.mongolab.com:33629/todoapp'
-	url: 'mongodb://localhost/todoApp'
+	url: 'mongodb://dbuser:dbpass@ds033629.mongolab.com:33629/todoapp'
 };
